a6ac70d16c5efae7b1a73e415bf6103fb152eb3b
repping <r.eppingbroek@outlook.com>
2023-02-20 10:40:37 +0100

    init

---
+++ providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "2.26.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = var.do-token
+}
---
+++ variables.tf
@@ -0,0 +1,3 @@
+variable "do-token" {
+  description = "Digital Ocean api token."
+}
